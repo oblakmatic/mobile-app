a0beb3ad9d92cd81152f8e91bf25eb9180a9819a
Unknown <mo2625@student.uni-lj.si>
2019-08-17 13:54:56 +0200

    nativescript base

---
+++ ns-mobile-app/src/main.ts
@@ -0,0 +1,6 @@
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { enableProdMode } from '@angular/core';
+import { AppModule } from "./app/app.module";
+
+enableProdMode();
+platformNativeScriptDynamic().bootstrapModule(AppModule);
